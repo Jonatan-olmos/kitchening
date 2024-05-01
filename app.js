d5c2a07c7c5f21c0dede85df465ade758ed314fc
jonatan <jon.ol.23.joo@gmail.com>
2024-05-01 20:30:43 -0300

    animacion

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const PORT = 3030;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname, 'views', 'carrito.html')));
+app.get('/detalle',(req,res) => res.sendFile(path.join(__dirname, 'views', 'detalle.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/registro',(req,res) => res.sendFile(path.join(__dirname, 'views', 'registro.html')));
+
+
+
 
 app.listen(PORT,( )=> (console.log(`servidor corriendoen http://localhost:${PORT}`)));
