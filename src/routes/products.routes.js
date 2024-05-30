c9ae3e3a0bf9c7ed6099105bd652a27508ef0773
jonatan olmos <jon.ol.23.joo@gmail.com>
2024-05-30 18:50:51 -0300

    multer

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { detail, add, edit, create, update, remover } = require('../controllers/productsController');
+const upload = require('../middleware/upload');
 
 
 /* /productos */
@@ -8,7 +9,7 @@ router
 .get('/detalle/:id', detail)
 .get('/agregar', add)
 .get('/editar/:id', edit)
-.post('/crear', create)
+.post('/crear',upload.single('mainImage') ,create)
 .put('/actualizar/:id', update)
 .delete('/eliminar/:id', remover)
 
