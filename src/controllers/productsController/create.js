0f41515af65663e45c579201d980115e1dcc3802
jonatan olmos <jon.ol.23.joo@gmail.com>
2024-06-16 11:21:41 -0300

    crud

--- src/controllers/productsController/create.js
+++ src/controllers/productsController/create.js
@@ -2,9 +2,12 @@ const { leerJSON, escribirJSON } = require("../../data")
 const Product = require("../../data/Product")
 
 module.exports = (req,res) => {
-   const {name, address,url_map,description,category   } = req.body;
+   const {name, address,url_map,description,category,   } = req.body;
 
-   const newProduct = new Product(name,address,url_map,description,category)
+   const mainImage = req.file;
+
+
+   const newProduct = new Product(name,address,url_map,description,category, mainImage)
    const products = leerJSON('products');
 
 products.push(newProduct);
